b3efb9a90d06d558705e5d6df76e85ece115fee8
Yoshimura Yuu <yyu@mental.poker>
2018-04-27 02:26:08 +0900

    Fix #11

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ cp -r ./book/second-edition/src/img ./
 for f in ./img/*.svg
 do
   if [[ $f =~ \./img/(.*)\.svg ]]; then
-    inkscape -z -D --file=`pwd`/img/${BASH_REMATCH[1]}.svg --export-pdf=`pwd`/${BASH_REMATCH[1]}.pdf --export-latex=`pwd`/${BASH_REMATCH[1]}.pdf_tex
+    inkscape -z -D --file=`pwd`/img/${BASH_REMATCH[1]}.svg --export-pdf=`pwd`/${BASH_REMATCH[1]}.pdf
+    # inkscape -z -D --file=`pwd`/${BASH_REMATCH[1]}.pdf --export-latex=`pwd`/${BASH_REMATCH[1]}.pdf_tex
   fi
 done
 
@@ -20,4 +21,3 @@ do
     FILENAME="${BASH_REMATCH[1]}.md" pandoc -o "./target/${BASH_REMATCH[1]}.tex" -f markdown_github+footnotes+header_attributes-hard_line_breaks-intraword_underscores --pdf-engine=lualatex --top-level-division=chapter --listings --filter ./filter.py $f
   fi
 done
-
