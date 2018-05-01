e6ba094b86dcc7f655fae6a30b2572e97ce7e92a
Yoshimura Yuu <yyu@mental.poker>
2018-05-01 22:52:48 +0900

    Fix foreword position

--- Makefile
+++ Makefile
@@ -5,5 +5,5 @@ all : book.pdf
 clean :
 	latexmk -C
 
-book.pdf : book.tex
+book.pdf : book.tex body.tex
 	latexmk -pdf book.tex
--- body.py
+++ body.py
@@ -5,12 +5,13 @@ import sys, re
 
 if __name__ == "__main__":
     print('\\frontmatter')
+    print('\\input{./target/foreword.tex}')
     print('\\tableofcontents')
     print('\\mainmatter')
     print('\\setcounter{secnumdepth}{3}')
     
     for line in sys.stdin:
         r = re.search(r'\[.*\]\((.*)\.md\)$', line)
-        if r is not None:
+        if r is not None and not r.group(1) == 'foreword':
             src = r.group(1)
             print('\\input{./target/' + src + '.tex}')
